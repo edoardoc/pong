65605b23f5bc098bc199f02747a499ff4e987eb2
Edoardo Ceccarelli <ridleys@gmail.com>
2022-10-17 08:53:15 +0300

    todo

--- broadcaster/antenna/antenna.go
+++ broadcaster/antenna/antenna.go
@@ -75,21 +75,7 @@ func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, s
 		fd := data["fullDocument"]
 		fmt.Println(fd)
 		fmt.Printf("type is %T", fd)
-
-		username, ok := fd["username"].(string)
-
-		for k, v := range data.(primitive.M)["fullDocument"] {
-			if str, ok := v.(string); ok {
-				fmt.Println(str)
-				// Use k and str
-			}
-		}
-
-		// fmt.Println(ok)
-		// fmt.Println(md)
-
-		// newChannel := data["channel"]
-		// log.Printf("starting trasmission of channel %v\n", newChannel)
+		// TODO: need to unmarshal this
 
 	}
 }
@@ -153,7 +139,5 @@ func main() {
 			log.Print("ADIOS")
 		}()
 	}))
-
 	waitGroup.Wait()
-
 }
